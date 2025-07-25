ac72475d870446ee8bf6426de4380b106e1e1ca1
luke.gumbleton <luke.gumbleton@haymarket.com>
2025-07-25 20:18:16 +0100

    Docker update

--- obj/Debug/net9.0/Portfoliodocker.AssemblyInfo.cs
+++ obj/Debug/net9.0/Portfoliodocker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfoliodocker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b42cb35e8c2575ab44e6f426b13700cdcc51bcee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b971ab6d4f5ec279f9883c8e0bb733a785ea093")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfoliodocker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfoliodocker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
